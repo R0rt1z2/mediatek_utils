abedb09834b7da42e644a5f0e6137657469b1695
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-03-02 21:17:26 +0100

    Improve the handshake script

--- handshake_mtk.py
+++ handshake_mtk.py
@@ -7,65 +7,117 @@ import glob
 import os.path
 import serial.tools.list_ports
 
-magic = b'\xa0\x0a\x50\x05\xa0\x0a\x50\x05\xa0\x0a\x50\x05' # magic
+GET_VERSION = b"\xff"
+GET_BL_VER  = b"\xfe"
+GET_HW_VER  = b"\xfc"
+GET_HW_CODE = b"\xfd"
 
-hwid_list = {
+HWID_LIST = {
    "MT65xx" : "2000",
    "MT6627" : "0003"
 }
 
+def write(s, command):
+        s.flushInput()
+        s.write(command)
+        s.flush()
+        return s.read(len(command))
+
+def manage_ports(s, action):
+     # Closes and opens specified port. s = port, action = close/open #
+     if action is "close":
+         s.close()
+         print("[?] {} closed succsesfully!\n".format(sys.argv[2]))
+         sys.exit(0)
+     elif action is "open":
+         print("[?] Opening {}...".format(s))
+         s = serial.Serial(s)
+         print("[+] Port Opened correctly!")
+         return s
+     else:
+         print("[-] Unknown action: {}".format(action))
+
+def serial_check(s, bytes, len): # serial, bytes, int
+     # Checks specified bytes with a serial read with the specified lenght #
+     print("[?] Checking bytes...") 
+     bytes_to_check = s.read(len)
+     if bytes_to_check != bytes:
+            raise RuntimeError("ERROR: Device returned {} instead of expected data.\n".format(bytes_to_check))
+     else:
+            print("[?] Device returns: {}".format(bytes))
+
+def check_device(ports):
+    for port in ports:
+       portHwid = port.hwid
+       if HWID_LIST.get("MT6627") in portHwid:
+           print("\n[?] BootROM connected? {}".format(portHwid))
+           device = "bootROM"
+           return device
+       elif HWID_LIST.get("MT65xx") in portHwid:
+           print("\n[?] Preloader connected? {}".format(portHwid))
+           device = "Preloader"
+           return device
+       else:
+           print("\n[?] Unknown device connected? {}".format(portHwid))
+           device = "Unknown" 
+           return device
+
+def handshake(s, device):
+     # Writes the magic cmd to the specified port to handshake it #
+     print("[?] Try to handshake the device...")
+     if device == "Unknown":
+           print("[!] Attempt to write to an unknown device...")
+
+     try:
+       while True:
+          b = write(s, b'\xa0')
+          if b == b'\x5f':
+             break
+
+       print("[?] Complete the sequence...")
+       write(s, b'\x0a')
+       write(s, b'\x50')
+       write(s, b'\x05')
+
+       print("[+] Handshake Complete!")
+
+     except (OSError, serial.SerialException):
+          print("[-] FATAL: Error during the handshake of the device...")
+         
 def main():
-     """
-        USAGE:
-           handshake_mtk.py -d /dev/ttyACM* (if you're in win use COMX)
-     """
-     if sys.argv[1] == "-d":
-          print("\n[?] Waiting for the device...\n")
+
+     if sys.argv[1] == "-h":
+          print("\n                              MediaTek Handshake Tool\n")
+          print("         preloader_tool.py -d /dev/ttyACM0 --> Handshake the specified port\n")
+
+     elif sys.argv[1] == "-d":
+          sys.stdout.write("\n[?] Waiting for the device...")
+
           while os.path.exists(sys.argv[2]) != True:
                sys.stdout.write("."); sys.stdout.flush()
                time.sleep(0.1)
-          ports = list(serial.tools.list_ports.comports())
-          for port in ports:
-              portHwid = port.hwid
-          if hwid_list.get("MT6627") in portHwid:
-             print("\n\n[?] BootROM connected? {}".format(portHwid))
-             device = "bootROM"
-          elif hwid_list.get("MT65xx") in portHwid:
-             print("\n\n[?] Preloader connected? {}".format(portHwid))
-             device = "Preloader"
-          else:
-             print("\n\n[?] Unknown device connected? {}".format(portHwid))
-             device = "Unknown"          
-          print("[?] Opening {}...".format(sys.argv[2]))
-          s = serial.Serial(sys.argv[2])
+
+          ports = list(serial.tools.list_ports.comports())   
+      
+          device = check_device(ports)
+
+          s = manage_ports(sys.argv[2], "open")
           time.sleep(0.10)
-          print("[+] Port Opened correctly (yay!)")
-          print("[?] Try to handshake the device...")
-          if device is "Unknown":
-              print("[!] Attempt to write to an unknown device...")
-          s.write(magic) # Write magic
-          s.write(magic) # Complete the sequence
-          bytes = s.read(5)
+
+          handshake(s, device)
+
           if device is "bootROM":
-             print("[?] Device returns: {}".format(bytes))
-             print("[+] Handshake Complete!")
-             print("[?] {} closed succsesfully!\n".format(sys.argv[2]))
-             s.close()
-             sys.exit(0)
+              manage_ports(s, "close")
+
           else:
-              print("[?] Checking recieved bytes...")
-              if bytes == b'READY':
-                 print("[?] Device returns: {}".format(bytes))
-                 print("[+] Handshake Complete!")
-                 print("[?] {} closed succsesfully!\n".format(sys.argv[2]))
-                 s.close()
-                 sys.exit(0)
-              else:
-                 raise RuntimeError("ERROR: Device returned {} instead of expected data. Maybe the device is already handshaked?\n".format(bytes))
-                 s.close()
-                 sys.exit(1)
+              #serial_check(s, b'READY', 5)
+              if sys.argv[1] == "-d":
+                 manage_ports(s, "close")
+     else:
+          print("\n[-] Invalid option: {}\n".format(sys.argv[1]))
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-       raise RuntimeError("[-] Expected more arguments")
+       print("[-] Expected more arguments\n")
     main()
