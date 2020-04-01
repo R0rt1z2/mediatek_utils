5b42b6134315ee5e6322e18863e0d6dbee0b0db1
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-04-01 15:14:12 +0200

    Update PLInfo.py

--- PLInfo.py
+++ PLInfo.py
@@ -5,10 +5,11 @@ import struct
 import sys
 import traceback
 
-MTK_BLOADER_INFO = b'MTK_BLOADER_INFO'
-ROM_INFO = b'ROMINFO_v'
-DATE_BYTES = b'Build'
-HEADER_BYTES = b'EMMC_BOOT'
+MTK_BLOADER_INFO = "4d544b5f424c4f414445525f494e464f"
+ROM_INFO = "524f4d494e464f5f76"
+DATE_BYTES = "4275696c64"
+HEADER_BYTES = "454d4d435f424f4f54"
+
 EMMC_FLASH_IMAGE = 0x0
 MEDIATEK_BOOT_HEADER = 0x800
 BOOT_SECTION_START = 0x214
@@ -17,22 +18,25 @@ MEDIATEK_FILE_INFO = 0x808
 def check_emmc_boot(f):
     f.seek(0) # first byte
     header = f.read(0x9)
-    print("Preloader Header: {}".format(header.decode("utf-8")))
-    if header != HEADER_BYTES:
+    if header != parse_hex(HEADER_BYTES):
         header = False
         return header
     else:
+        print("Preloader Header: {}".format(header.decode("utf-8")))
         header = True
         return header
 
+def parse_hex(hex):
+    return bytes.fromhex(hex)
+
 def read_date(f, data):
-    offset = data.find(DATE_BYTES)
+    offset = data.find(parse_hex(DATE_BYTES))
     f.seek(offset + 16)
     date = f.read(15)
     print("Preloader Date: {}".format(date.decode("utf-8")))
 
 def read_bloader_info(f, data):
-    offset = data.find(MTK_BLOADER_INFO)
+    offset = data.find(parse_hex(MTK_BLOADER_INFO))
     f.seek(offset + 17) # len(MTK_BLOADER_INFO) + '_'
     bloader_info = f.read(3)
     print("BLOADER_INFO Version: {}".format(bloader_info.decode("utf-8")))
@@ -41,7 +45,7 @@ def read_bloader_info(f, data):
     print("Preloader Name: {}".format(pl_name.decode("utf-8")))
 
 def read_platform(f, data):
-    offset = data.find(ROM_INFO)
+    offset = data.find(parse_hex(ROM_INFO))
     f.seek(offset + 9)
     f.read(7)
     platform = f.read(6)
