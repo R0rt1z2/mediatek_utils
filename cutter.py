673c78a95c1491a172bace4fd15ac25d61d0d9c3
R0rt1z2 <rogerortizleal@gmail.com>
2020-06-17 23:41:25 +0200

    cutter: Add support to cut pl aswell.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- cutter.py
+++ cutter.py
@@ -6,32 +6,39 @@
 import sys
 import os.path
 
-HEADER = b'\x07\x00\x00\xea'
+LK_HEADER = b'\x07\x00\x00\xea'
+PL_HEADER = 0x800
 
 def get_offset(data):
-    offset = data.find(HEADER)
+    offset = data.find(LK_HEADER)
     if offset == -1:
         print("[-] Cannot find the end of the header! Is this an LK?")
         exit(1)
     print("[?] Header size is {} bytes.".format(offset))
     return offset
 
-def cut_header(in_file, out_file, offset):
-    with open(in_file, 'rb') as in_file:
-        with open(out_file, 'wb') as out_file:
+def cut_header(image, out, offset):
+    with open(image, 'rb') as in_file:
+        with open(out, 'wb') as out_file:
            out_file.write(in_file.read()[offset:])
-    print("[+] Header Remove Complete. Output: cutted_lk.img!")
+    print("[+] Header Remove Complete. Output: {}!".format(out))
 
 def main():
     if os.path.isfile(file):
         with open(file, "rb") as lk:
             data = lk.read()
-        offset = get_offset(data)
-        cut_header(file, "cutted_lk.img", offset)
+        if "lk" in sys.argv[1]:
+            offset = get_offset(data)
+            cut_header(file, "cutted_lk.img", offset)
+        elif "pl" in sys.argv[1]:
+            cut_header(file, "cutted_pl.img", PL_HEADER)
+        else:
+            print("[?] USAGE: python3 cutter.py lk/pl image.img")
+            exit(1)
     else:
         print("[-] Cannot open {}!".format(file))
         exit(1)
 
 if __name__ == '__main__':
-    file = sys.argv[1]
+    file = sys.argv[2]
     main()
